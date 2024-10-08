5fc066d928f32d496b1658b0652016690f187fdb
szellevente <szel.levente@diak.szbi-pg.hu>
2024-10-08 10:47:40 +0200

    Update files

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render.template('index.html', name=name)
+    name = None
+    if request.method=="POST":
+        name = request.form.get('porduct_name')
+    return render_template('index.html', name=name)
 
 
-    if __name__== "__main__":
-        app.run(debug=True)
+if __name__== "__main__":
+    app.run(debug=True)
