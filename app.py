d238f9296524031dc02e4378817f894e73eb5036
szellevente <szel.levente@diak.szbi-pg.hu>
2024-10-15 09:55:27 +0200

    Add list products function

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+products=[]
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
+    
     if request.method=="POST":
         name = request.form.get('porduct_name')
-    return render_template('index.html', name=name)
+        products.append(name)
+    return render_template('index.html', products_=products)
 
 
 if __name__== "__main__":
